ae1893e9b53e106fd8c552371583b2d37f969c34
yunxiang.gong <gyx_edu@qq.com>
2019-09-26 16:33:29 +0800

    1. update for compile

--- src/libb64/cencode.c
+++ src/libb64/cencode.c
@@ -5,7 +5,8 @@ This is part of the libb64 project, and has been placed in the public domain.
 For details, see http://sourceforge.net/projects/libb64
 */
 #ifdef USE_FEMBED_LWIP
-#define CORE_HAS_LIBB64#elif defined(ESP8266)
+#define CORE_HAS_LIBB64
+#elif defined(ESP8266)
 #include <core_esp8266_features.h>
 #endif
 
